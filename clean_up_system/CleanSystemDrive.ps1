0874b72fe29a35d22cd8621b3309af5df6d57d5d
Ruslan Dzhumaiev <61501416+dzhumaiev@users.noreply.github.com>
2026-06-17 19:24:50 +0200

    Update CleanSystemDrive.ps1

--- clean_up_system/CleanSystemDrive.ps1
+++ clean_up_system/CleanSystemDrive.ps1
@@ -1,1 +1,17 @@
+# Clean Adobe from Installer
+$AdobeInstallers = (ls -File C:\Windows\Installer\ | ?{((Get-AuthenticodeSignature $_.FullName).SignerCertificate.Subject) -like "*adobe*"})
+Get-WmiObject -Class Win32_Product | ?{$_.Name -like "*adobe*"} | %{
+    $AdobeOrigFile += @($_.LocalPackage -replace ".*\\",'')
+}
+
+$AdobeInstallers | rm -Force -Exclude $AdobeOrigFile 2>$null
+
+# Clean SoftwareDistribution\Download
+ls 'C:\Windows\SoftwareDistribution\Download' | ?{$_.LastWriteTime -lt (Get-Date).AddDays(-30)} | rm -Force -Recurse 2>$null
+
+# Clean WinSxS
+$DismEXE = (Dism.exe /Online /Cleanup-Image /AnalyzeComponentStore)
+if (($DismEXE | sls 'Component Store Cleanup Recommended : Yes')) {
+    Dism.exe /Online /Cleanup-Image /StartComponentCleanup
+}
 
